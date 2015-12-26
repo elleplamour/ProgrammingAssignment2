bfd5e687e421d1009fbe9229b6befa74394e946b
elleplamour <recherchellize88@gmail.com>
2015-12-26 16:55:00 +0000

    Added solutions and descriptions.
    Coursera Programming assignment 2 - "Lexical Scoping" edited version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+Creating a function to calculate cache matrix.
 
+makecachematrix <- function(x = matrix()) {
+    # mat = matrix    
+} mat <- NULL
+set <- function(n) {
+    x <<- n
+    # set is a function(n). n will then be x <<- n assigend to x.   
 }
 
+# get contains an empty function. Inverses functions get and set are created where inverse was assigned to mat.
+get <- function() x 
+setinverse <- function(inverse) mat <<- inverse 
+getinverse <- function() mat 
+list(set = set, go = go, setinverse = setinverse, getinverse = getinverse)
+
+# Above will just take a list of previous functions created.
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+Creating a function which calculates the inverse(X)
+
+cachesolve <- function(x, ...) {
+    # mat = for matrix, getting inverse(X)  
+    mat <- x$getinverse()
+    if(!is.null(mat)) {
+        return(mat)
+        # return will show the value for mat.
+    }
+    files <- x$get()
+    mat <- solve(files)
+    x$setinverse(mat)
+    mat
+    # mat will now contain the cachematix/solve values.
 }
